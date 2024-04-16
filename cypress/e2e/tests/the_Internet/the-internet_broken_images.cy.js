36e1ea476ac26c740248eed1823b18e7ae03c8d9
2i-Gyorgy <Gyorgy.Papp@2itesting.com>
2024-04-16 16:03:47 +0100

    file created

--- cypress/e2e/tests/the_Internet/the-internet_broken_images.cy.js
+++ cypress/e2e/tests/the_Internet/the-internet_broken_images.cy.js
@@ -1,25 +1,8 @@
-describe("Test Basic Authentication Functionality", function () {
-  it("logs in by appending username and password in URL", function () {
-    cy.visit("https://admin:admin@the-internet.herokuapp.com/basic_auth");
-    cy.get("p").should(
-      "include.text",
-      "Congratulations! You must have the proper credentials."
-    );
-  });
+describe("Test Images", function () {
+  it("makes checks if image exist", function () {
+    // visit page in question for testing
+    cy.visit("https://the-internet.herokuapp.com/broken_images");
 
-  it("logs in by using headers", function () {
-    cy.visit("https://the-internet.herokuapp.com/basic_auth", {
-      headers: {
-        authorization: "Basic YWRtaW46YWRtaW4=",
-      },
-      failOnStatusCode: false,
-    });
-    cy.get("p").should(
-      "include.text",
-      "Congratulations! You must have the proper credentials."
-    );
+    //
   });
 });
-
-// https://testersdock.com/cypress-basic-auth/
-// I don't consider this to be a good test, as both solutions will bypass the basic auth popup, therefore not perform a life-like test.
