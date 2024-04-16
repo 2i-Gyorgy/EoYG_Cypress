e50dda068177386c60f63a53bc27aeaaf8a8ef77
2i-Gyorgy <Gyorgy.Papp@2itesting.com>
2024-04-16 14:27:58 +0100

    test spec tidied up

--- cypress/e2e/tests/the_Internet/the-internet_checkboxes_spec.cy.js
+++ cypress/e2e/tests/the_Internet/the-internet_checkboxes_spec.cy.js
@@ -1,16 +1,23 @@
-describe('Test checkbox', () => {
-  it('visits checkboxes page and plays around with check boxes', () => {
-    cy.visit('https://the-internet.herokuapp.com/checkboxes')
-    // Get the checkbox, check if it exists
-    cy.get("input")
-    .should('exist')
-    .and('have.attr', 'type')
+describe("Test Checkbox functionality", function () {
+  it("clicks check boxes", () => {
+    // visit page in question for testing
+    cy.visit("https://the-internet.herokuapp.com/checkboxes");
 
-    // click the button
-    cy.get("input")
-    .click({ multiple: true })
+    // First Checkbox should be Enabled and Not Checked
+    cy.get('[type="checkbox"]')
+      .first()
+      .should("be.enabled")
+      .and("not.be.checked");
 
-    // check if it added new elemnt
-    // cy.get('.added-manually').should('exist')
-  })
-})
+    // Second Checkbox should be Enabled and Checked
+    cy.get('[type="checkbox"]').eq(1).should("be.enabled").and("be.checked");
+
+    // Click the first Chekbox and assert that it is cheked
+    // cy.get('[type="checkbox"]').first().check();
+    cy.get('[type="checkbox"]').first().click().should("be.checked");
+
+    // Click the second Chekbox and assert that it is Not Cheked
+    // cy.get('[type="checkbox"]').first().check();
+    cy.get('[type="checkbox"]').eq(1).click().should("not.be.checked");
+  });
+});
